9f7c6fbf06e62ed5072b505727766c87698a0ea3
volekerb <volekerb@users.noreply.github.com>
2021-10-26 08:43:40 +0000

    deploy: c0b511a070441dc61a016a8147fc5d8058d0700c

---
+++ precache-manifest.45d8e46e25badf31d32acfbd5063bc3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "245e82be0b23dc14812f8db3707c2b68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4ca7df3e1990a7548f6",
+    "url": "/static/css/main.ee392d7f.chunk.css"
+  },
+  {
+    "revision": "16e9b53e71c585b53ba0",
+    "url": "/static/js/2.10fb1d42.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.10fb1d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4ca7df3e1990a7548f6",
+    "url": "/static/js/main.6150cc2b.chunk.js"
+  },
+  {
+    "revision": "a11ca162747efa5dc89a",
+    "url": "/static/js/runtime-main.be12ddd9.js"
+  }
+]);
